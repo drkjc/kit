b5b609043c297593cfba8ebf03e2ae3fdcd5587b
Derek Cerretani <drkjc@protonmail.com>
2019-08-21 17:08:08 -0700

    users can soft login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     def index 
     end
 
+    def new 
+        @user = User.new
+    end
+
     def show 
         @user = User.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  
   resources :rooms do 
     resources :messages    
   end
-  
+
   resources :users do
     resources :rooms 
   end
