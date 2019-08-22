1fa097e0d2f5a2bcbcd7183f7285e9a90e7d25dc
Derek Cerretani <drkjc@protonmail.com>
2019-08-21 19:29:12 -0700

    extended friendly Id to rooms

--- db/migrate/20190822004734_add_column_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToRooms < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rooms, :dm_id, :integer, :default => 0
-  end
-end
--- db/migrate/20190822020237_add_user_id_column_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdColumnToRooms < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rooms, :user_id, :integer, :default => 0
-  end
-end
