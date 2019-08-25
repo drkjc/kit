2c78f4f5b3082534a1a2d29e7e6f0844f4b89a1d
Derek Cerretani <drkjc@protonmail.com>
2019-08-25 14:24:41 -0700

    fixed room create path and users index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,30 +3,43 @@ class RoomsController < ApplicationController
     skip_before_action :verify_authenticity_token
     
     def index
-        @rooms = Room.all
-        @room = Room.new
-        render json: @rooms, status: 201
+      @rooms = Room.all
+      @room = Room.new
+      render json: @rooms, status: 201
     end 
 
     def new
 
     end 
 
-    def create 
+    def create
+      if params[:user_name] 
+        room_name = @user.name + '-' + params[:user_name]
+        room = Room.friendly.find_by(name: room_name)
+        if room 
+          redirect_to rooms_path(room)
+        else
+          room = @user.rooms.build(name: room_name)
+          if @user.save 
+            redirect_to rooms_path(room)
+          end
+        end  
+      else
         room = @user.rooms.build(name: params[:channel][:name])
         room.name = room.name.split.join('-')
-        if @user.save
-            binding.pry
-            render json: room, status: 201
-        else
-            render json: room, status: :bad_request
+        if @user.save 
+          render json: room, status: 201
         end
+      end
     end 
 
     def show 
-        room = Room.friendly.find(params[:id])
-        @message = Message.new
-        render json: room
+      room = Room.friendly.find(params[:id])
+      @message = Message.new
+      respond_to do |format|
+        format.html
+        format.json { render :json => room.to_json }
+      end
     end 
 
     def edit 
