83c29ca28a6acc63a7616a694ab73a3f5905a0fc
Derek Cerretani <drkjc@protonmail.com>
2019-09-10 15:54:18 -0700

    login/signup pages styled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    #raise params.inspect
     @user = User.find_by(name: params[:user][:name])
       if @user 
         session[:user_id] = @user.id
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController 
     before_action :find_user, only: [:index, :show, :search, :settings]
+    
 
     def show 
       @user = User.find(params[:id])
