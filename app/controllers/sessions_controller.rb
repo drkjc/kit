8e73258565f19244269926a9a1a47f193066cbe8
Derek Cerretani <drkjc@protonmail.com>
2019-09-11 12:50:30 -0700

    added github oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,38 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    binding.pry
-    @user = User.find_by(name: params[:user][:name])
-      if @user 
+    if params[:provider]
+      @user = User.find_or_create_by(email: oauth[:info][:email]) do |u|
+        u.name = oauth[:info][:name]
+        u.email = oauth[:info][:email]
+        u.password_digest = SecureRandom.hex 
+      end 
+      if @user.valid?
         session[:user_id] = @user.id
-          redirect_to home_path
+        redirect_to home_path 
       else 
         redirect_to signup_path
       end
+    else 
+      @user = User.find_by(name: params[:user][:name])
+      if @user 
+        session[:user_id] = @user.id
+        redirect_to home_path
+      else 
+        redirect_to signup_path
+      end
+    end
   end
 
   def destroy 
     log_out
     redirect_to root_path
+  end
+
+  private 
+
+  def oauth
+    request.env['omniauth.auth']
   end
 
 end
